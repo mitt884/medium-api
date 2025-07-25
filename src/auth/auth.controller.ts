d0f87d0b861668b83e550ced4d7c9b32cf169d1c
mitt884 <lethao080804@gmail.com>
2025-07-25 17:14:32 +0700

    add return types for async func

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -5,24 +5,25 @@ import { LoginDto } from './dto/login.dto';
 import { JwtAuthGuard } from '../common/guards/jwt-auth.guard';
 import { GetUser } from '../common/decorators/get-user.decorator';
 import { User } from '@prisma/client';
+import { UserResponse } from './interfaces/auth-response.interace';
 
 @Controller('api/users')
 export class AuthController {
   constructor(private authService: AuthService) {}
 
   @Post()
-  async register(@Body('user') registerDto: RegisterDto) {
+  async register(@Body('user') registerDto: RegisterDto): Promise<UserResponse> {
     return this.authService.register(registerDto);
   }
 
   @Post('login')
-  async login(@Body('user') loginDto: LoginDto) {
+  async login(@Body('user') loginDto: LoginDto): Promise<UserResponse> {
     return this.authService.login(loginDto);
   }
 
   @UseGuards(JwtAuthGuard)
   @Get('profile')
-  async getProfile(@GetUser() user: User) {
+  async getProfile(@GetUser() user: User): Promise<User> {
     return user; 
   }
 }
